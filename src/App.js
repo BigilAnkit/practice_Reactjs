c33e72ef0eaeab48b18189856609b98b2a8a27d1
BigilAnkit <gehlotankit19@gmail.com>
2024-02-02 17:16:37 +0530

    learn how to work modules import & export

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let name = "Ankit Gehlot";
+  let l = [10,20,30,40];
+  let obj={
+    'name':"ws",
+    'cName':"MERN",
+    'desc':"React Js"
+  }
+  let status = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>{name}</h1>
+      {l.map((v)=>{
+        return (
+          <div>{v}</div>
+        )
+      })}
+      <div>{obj.desc}</div>
+      {
+      (status) ?
+      <h1 style={{color:"red",backgroundColor:"yellow"}}>Welcome to Ankit Web</h1>
+      :
+      ""
+      
+      }
     </div>
   );
 }
